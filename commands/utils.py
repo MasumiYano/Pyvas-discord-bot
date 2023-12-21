803b31f634f41a725235db870506d9ef427be851
Masumi Yano <92832478+MasumiYano@users.noreply.github.com>
2023-12-21 01:20:18 -0800

    Update utils.py

--- commands/utils.py
+++ commands/utils.py
@@ -1,116 +1,49 @@
-def user_id_not_recognized() -> dict:
-    user_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "User Error.",
-                "value": "I didn't recognize that user id. You have to subscribe your user id first using `!substudent <user id>`. See [here link for readme.md].",
-                "inline": False
-            }
-        ],
-        "color": 0xFF0000
-    }
-
-    return user_error
+from .command import Command
 
 
-def user_id_already_exist(user_id: str) -> dict:
-    user_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "User Error.",
-                "value": f"The {user_id} already exists in your database.",
-                "inline": False
-            }
-        ],
-        "color": 0xFF0000
-    }
+class ShowHelp(Command):
+    async def execute(self, _):
+        help_details = {
+            "title": "Help ✅",
+            "description": "Commands List",
+            "fields": [
+                {
+                    "name": "`$substudent <user id>`",
+                    "value": "Subscribe your user id to pyvas. To get user id, see [here](https://github.com/MasumiYano/Pyvas-discord-bot#user-id).\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$subcourse <course name> <course id>`",
+                    "value": "Subscribe to the course that you want to keep track of the assignments. `course name` can be anything. To get course id, see [here](https://github.com/MasumiYano/Pyvas-discord-bot#course-id)\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$subhw <course name> <hw name> <hw id>`",
+                    "value": "Subscribe to the hw (Homework). The `hw name` can be anything. To get hw id, see [here](https://github.com/MasumiYano/Pyvas-discord-bot#how-to-get-assignment-id).\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$showcourse`",
+                    "value": "Show all the courses that you're enrolled.\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$showcoursenow`",
+                    "value": "Show all the courses for the quarter.\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$showhwgrade <course name> <hw name>`",
+                    "value": "Show the grade of your subscribed hw.\n",
+                    "inline": False
+                },
+                {
+                    "name": "\n`$showhwdue <course name> <hw name>`",
+                    "value": "Show the due date for your subscribed hw.\n",
+                    "inline": False
+                }
+            ],
+            "color": 0x00ff00
+        }
 
-    return user_error
-
-
-def discord_id_not_recognized() -> dict:
-    password_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "Discord ID Error",
-                "value": "You do not exist in my database. Make sure to subscribe you first!",
-                "inline": False
-            }
-        ],
-        "color": 0xff0000
-    }
-
-    return password_error
-
-
-def course_not_recognized() -> dict:
-    course_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "Course ID Error",
-                "value": "I didn't recognize that course. Please subscribe your course id first using `!subcourse <course_name> <course_id>.`",
-                "inline": False
-            }
-        ],
-        "color": 0xff0000
-    }
-
-    return course_error
-
-
-def course_already_exist(course_id: str) -> dict:
-    course_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "Course ID Error",
-                "value": f"Course {course_id} already exists in my database.",
-                "inline": False
-            }
-        ],
-        "color": 0xff0000
-    }
-
-    return course_error
-
-
-def assignment_not_recognized() -> dict:
-    assignment_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "HW Name Error",
-                "value": "I didn't recognize that hw name. Make sure to subscribe the hw first using `!subhw <course name> <hw name> <hw id>`",
-                "inline": False
-            }
-        ],
-        "color": 0xff0000
-    }
-
-    return assignment_error
-
-
-def assignment_already_exist(hw_name: str) -> dict:
-    assignment_error = {
-        "title": "Error ⚠️",
-        "description": "Oops, something is wrong...",
-        "fields": [
-            {
-                "name": "HW Name Error",
-                "value": f"{hw_name} already exists in my database."
-            }
-        ],
-        "color": 0xff0000
-    }
-
-    return assignment_error
+        return help_details
